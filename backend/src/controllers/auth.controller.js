dc1fdaddeed50a720fdffe09b4f884b74ac2f978
Abi-390 <dekaabinash07@gmail.com>
2026-01-09 00:03:38 +0530

    controllers initiated

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+ const userModel = require("../models/user.model")
+
+async function registerController(req,res) {
+    const {username,password} = req.body;
+
+    const isUserAlreadyExists = userModel.findOne({username});
+
+    if(isUserAlreadyExists){
+        return res.status(400).json({
+            message:"User already exists"
+        })
+    }
+}
+
+async function loginController(req,res) {
+    const {username,password} = req.body;
+}
+
+
+
+
+
+module.exports = {
+    registerController,
+    loginController
+}
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,31 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  const existingUser = await userModel.findOne({
-    username,
-  });
-
-  if (existingUser) {
-    return res.status(409).json({
-      message: "This username already exists",
-    });
-  }
-
-  const user = await userModel.create({
-    username,
-    password,
-  });
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  res.cookie("token", token);
-
-  res.status(201).json({
-    message: "User created successfully",
-    user,
-  });
-});
+router.post("/register",);
+router.post("/login",);
 
 module.exports = router;
