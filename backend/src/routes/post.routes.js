87d456a83da502e2a7e8862014592165b4f4487e
Abi-390 <dekaabinash07@gmail.com>
2026-01-16 20:50:22 +0530

    ai service and post controller created

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/auth.middleware")
+const createPostController = require("../controllers/post.controller")
 const router = express.Router();
 const multer = require("multer");
 
