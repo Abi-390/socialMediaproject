d38afd1c7cf04dece0c5987cb58012de845fdd12
Abi-390 <dekaabinash07@gmail.com>
2026-01-17 17:33:50 +0530

    removed localhost 5172 from cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173",  "https://captionary-gamma.vercel.app"],
+    origin:   "https://captionary-gamma.vercel.app",
     credentials: true,               
   }))
 
