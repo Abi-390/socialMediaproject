1c4466c844cd043a6c56fa2b403f4d23c5d9fa7f
Abi-390 <dekaabinash07@gmail.com>
2026-01-17 17:54:57 +0530

    Updated login controller allwoing cors access and httponly

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,7 +55,11 @@ async function loginController(req, res) {
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,       
+    sameSite: "none",   
+  });
 
   res.status(200).json({
     message: "User logged in successfully",
